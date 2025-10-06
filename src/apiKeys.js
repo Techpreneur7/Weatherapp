f004945132e8adb053703d80c966aafe179b72c0
Techpreneur7 <ravientrepreneur7@gmail.com>
2025-10-06 22:38:43 +0530

    second commit

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,8 @@
-module.exports = {
-      key: "1b1412d1f39a33b64cfe1c1b85149850",
-      base: "https://api.openweathermap.org/data/2.5/",
-    };
+
                  
+    const apiKeys = {
+  base: "https://api.openweathermap.org/data/2.5/",
+  key: process.env.REACT_APP_WEATHER_KEY,
+};
+
+export default apiKeys;
